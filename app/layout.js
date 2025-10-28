a94b75637267654870a88a8f63c23998e0286e67
ittehadrahman <ittehadrahman@iut-dhaka.edu>
2025-10-28 13:46:21 +0600

    change

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <main>{children}</main>
+      <body>{children}</body>
     </html>
   );
 }
